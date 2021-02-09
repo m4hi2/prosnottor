fac69111ddbe004726b6ec430ef900574c0cc0f6
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-02-09 19:13:11 +0600

    feat: qna form created

--- web_app/application.py
+++ web_app/application.py
@@ -8,3 +8,10 @@ bp = Blueprint('application', __name__, url_prefix='/')
 @bp.route('/', methods=('GET',))
 def index():
     return render_template('application/index.html.j2')
+
+
+@bp.route('/qna', methods=('GET', 'POST'))
+def qna():
+    if request.method == 'POST':
+        pass
+    return render_template('application/qna.html.j2')
