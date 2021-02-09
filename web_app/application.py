3e2af1dab7742956af4ed9065ba703c2a4a19a2d
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-02-09 18:38:31 +0600

    feat: created the index view

--- web_app/application.py
+++ web_app/application.py
@@ -3,3 +3,8 @@ import functools
 from flask import Blueprint, redirect, render_template, request
 
 bp = Blueprint('application', __name__, url_prefix='/')
+
+
+@bp.route('/', methods=('GET',))
+def index():
+    return render_template('index/index.html.j2')
